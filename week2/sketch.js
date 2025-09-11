d5642b9d9d716522484d7fe1d8cc33f8e79f40e8
xy2979 <xy2979@nyu.edu>
2025-09-10 22:09:31 -0400

    add shapes

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,10 +1,22 @@
 
 
 function setup() {
+  // runs once at the start
   createCanvas(windowWidth, windowHeight);
-  
+  //camelBackNotation
+  //createCanvas is a function that creates a canvas for our p5.js sketch to draw into. 
+  //it takes two parameters, width and height. windowwidth and windowheight are used to set the size to the full size of our browser window.
+  background(100, 121, 245);
 }
 
 function draw() {
+  // runs in a loop after setup
+  strokeweight(4);
   
+  circle(100, 200, 120);
+  //circle takes these parameters: x position, y position, diameter
+  ellipse(550, 190, 130, 70);
+  quad(360, 580, 360, 360, 100, 360, 100, 580);
+  
+
 }
