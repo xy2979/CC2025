c191e524b9cac0d2616cc8a591a17d332ad3ec3a
xy2979 <xy2979@nyu.edu>
2025-10-01 14:47:56 -0400

    added notes in week5a

---
+++ week5a/sketch.js
@@ -0,0 +1,12 @@
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  stroke(255);//white stroke
+  noFill();
+  circle(mouseX, mouseY, 25);
+}
