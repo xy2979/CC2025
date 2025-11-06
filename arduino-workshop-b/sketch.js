46a5ee74ef38b1840544f3bfcc20ae6179334dee
xy2979 <xy2979@nyu.edu>
2025-11-05 21:39:52 -0500

    added noted of arduino

---
+++ arduino-workshop-b/sketch.js
@@ -0,0 +1,70 @@
+// graphs sensor data from an analog 
+// sensor on A0 to the window
+
+let port; // object to hold serial port
+let c; // button
+let potentiometer = 0;
+let photoCell = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //colors
+  colorMode(HSB);
+  // create instance of the lib
+  port = createSerial();
+
+  // ports can be opened via a dialog after
+  // user interaction (see connectBtnClick below)
+  c = createButton('Connect to Arduino');
+  c.position(10, 10);
+  c.mousePressed(connectBtnClick);
+}
+
+function draw() {
+  background(220, 100, 50);
+  // read serial bufffer
+  let str = port.readUntil("\n"); // \n means a new line
+  // get rid of whitespace
+  //splits the values according to "space" character
+  //this creates an array called sensorValues
+  let sensorValues = str.split(" ");
+
+  // if there's valid data
+  if (str.length > 0) {
+    potentiometer = sensorValues[0];
+    photoCell = sensorValues[1];
+  }
+
+  text("Potentiometer: " + potentiometer, 10, 50);
+  text("photocell: " + photoCell, 10, 70);
+
+  let circleD = map(potentiometer, 0, 4095, 0, width);
+  let hue = map(potentiometer, 0, 4095, 0, 360);
+
+  let brightness = map(photoCell, 700, 2800, 0, 100);
+
+  fill(hue, brightness, 100);
+  
+  //fill(hue, 100, 100);
+
+  circle(width/2,height/2,circleD);
+
+
+  // changes button label based on connection status
+  if (!port.opened()) {
+    c.html('Connect to Arduino');
+  } else {
+    c.html('Disconnect');
+  }
+}
+// if the connect button is clicked and there's
+// no connection, look for something named
+// "Arduino"
+function connectBtnClick() {
+  if (!port.opened()) {
+    port.open('Arduino', 9600);
+    
+  } else {
+    port.close();
+  }
+}
