d38e08607870161ec4a3c32456e7cb045526b756
xy2979 <xy2979@nyu.edu>
2025-11-12 15:59:47 -0500

    added ml5js folders at class

---
+++ ml5-hand-pose/sketch.js
@@ -0,0 +1,90 @@
+/*
+ * 👋 Hello! This is an ml5.js example made and shared with ❤️.
+ * Learn more about the ml5.js project: https://ml5js.org/
+ * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+ *
+ * This example demonstrates hand tracking on live video through ml5.handPose.
+ */
+
+let handPose; //variable to store the model
+let video; //variable to store video frames
+let hands = []; //array to store hand poses (up to 2)
+let pinch = 10000;
+let pinched = false;
+
+let stars = [];
+
+function preload() {
+  // Load the handPose model
+  handPose = ml5.handPose();
+}
+
+function setup() {
+  createCanvas(640, 480);
+  // Create the webcam video and hide it
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  // start detecting hands from the webcam video
+  handPose.detectStart(video, gotHands);
+  //background(0);
+}
+
+function draw() {
+  // Draw the webcam video
+  image(video, 0, 0, width, height);
+
+  // Draw all the tracked hand points
+  // for (let i = 0; i < hands.length; i++) {
+  //   let hand = hands[i];
+  //   for (let j = 0; j < hand.keypoints.length; j++) {
+  //     let keypoint = hand.keypoints[j];
+  //     fill(0, 255, 0);
+  //     noStroke();
+  //     textSize(20);
+  //     // if (i == 0) {
+  //     //   fill(255, 0, 0);
+  //     // } else {
+  //       fill(0, 255, 0);
+  //     //}
+
+  //     text(j, keypoint.x, keypoint.y);
+  //   }
+  // }
+
+  if (hands.length > 0) {// are there hands currently being tracked
+    //circle(hands[0].keypoints[8].x, hands[0].keypoints[8].y, 10);
+    let indexTip = hands[0].keypoints[8]; //store the index fingertip
+    let thumbTip = hands[0].keypoints[4];
+    let centerX = lerp(indexTip.x, thumbTip.x, 0.5);
+    let centerY = lerp(indexTip.y, thumbTip.y, 0.5);
+    pinch = dist(indexTip.x, indexTip.y, thumbTip.x, thumbTip.y);
+    if (pinch < 10) { //this threshold should be scaled to accomodate different depths
+      strokeWeight(10);
+      if (pinched == false) {
+        let coord = createVector(centerX, centerY);
+        stars.push(coord);
+        //pinched = true;
+      }
+    } else {
+      strokeWeight(1);
+      //pinched = false;
+    }
+    circle(centerX, centerY, pinch);
+
+    for (i = 0; i < stars.length; i++){
+      circle(stars[i].x, stars[i].y, 10);
+    }
+  }
+  // let tint = map(pinch, 100, 0, 0, 255);
+  // fill(255, tint);
+  // rect(0, 0, width, height);
+
+
+}
+
+// Callback function for when handPose outputs data
+function gotHands(results) {
+  // save the output to the hands variable
+  hands = results;
+}
