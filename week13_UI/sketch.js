b62d50139d94584e63c1958121d881ae0ebd8722
xy2979 <xy2979@nyu.edu>
2025-11-28 15:38:32 -0500

    added notes in final ui

--- week13_UI/sketch.js
+++ week13_UI/sketch.js
@@ -6,28 +6,54 @@ let monoPicker;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(250);
+  background(10);
+  fill("white");
+  //title
+  textSize(40);
+  textFont("Gamja Flower");
+  text('Silent Strings', 30, 50);
+  textSize(19);
+  textFont("Schoolbell");
+  //text for instruction
+  text('Thumb + Index Open = Show Pattern', 600, 100);
+  text('Pinch Thumb + Index Together = Change Pattern', 600, 130);
   
-  //adjust thickness
-  thicknessSlider = createSlider(1, 50, 1); // 最细1px，最粗20px，初始1
-  thicknessSlider.position(150,150);
+  //text for sliders/////////////////////
+  textSize(15);
+  text('Stroke Weight', 600, 190);
+  text('Opacity', 600, 220);
+  text('Color Style', 600, 250);
   
-  //adjust transparency
-  alphaSlider = createSlider(10, 255, 200);
-  alphaSlider.position(150, 250);
+  //create stroke weight slider///////////////////
+  thicknessSlider = createSlider(1, 15, 3); 
+  thicknessSlider.position(710, 175);
+  thicknessSlider.style('accent-color', '#53f6caff');
   
-  //adjust color style
+  //create opacity slider///////////////////////
+  alphaSlider = createSlider(10, 255, 100);
+  alphaSlider.position(660, 205);
+  alphaSlider.style('accent-color', '#53f6caff');
+  
+  //Create color style radio button////////////////
   colorStyleRadio = createRadio();
-  colorStyleRadio.position (150, 350);
+  colorStyleRadio.position(680, 235);
+  //font color
+  colorStyleRadio.style('color', '#ffffffff');
+  //radio color
+  colorStyleRadio.style('accent-color', '#53f6caff');
+  //font
+  colorStyleRadio.style('font-family', 'Schoolbell');
+  //font size
+  colorStyleRadio.style('font-size', '14px');
+  colorStyleRadio.style('margin', '6px 10');  
   colorStyleRadio.option("Monochrome");
   colorStyleRadio.option("Random");
   // default value is Monochrome
   colorStyleRadio.selected("Monochrome");
   
-  
   //picker initialization
-  monoPicker = createColorPicker('red');
-  monoPicker.position(150, 390);
+  monoPicker = createColorPicker('white');
+  monoPicker.position(700, 260);
 
 }
 
@@ -45,5 +71,5 @@ function draw() {
   stroke(c);
   noFill();
   
-  line(windowWidth, windowHeight, mouseX, mouseY);
+  //line(windowWidth, windowHeight, mouseX, mouseY);
 }
